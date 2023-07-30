96a2081d38d1015a669d313900cf420285d0436c
Harsh.Anand <harshanand27@gmail.com>
2023-07-30 17:11:19 +0530

    initil commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,92 @@
-import { Component } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('elementRefName', { static: true }) elementRef: any;
+
   title = 'jigsaw-puzzle';
+
+  puzzleImage = 'tree';
+
+  containerPos: any;
+
+  success: number = 0;
+
+  imagesList: any = [];
+
+  imagesSolve: any = [];
+
+  checked = false;
+
+  puzzleList = ['tree', 'rue', 'jungkook'];
+  
+  ngOnInit(): void {
+    this.generatePuzzle();
+  }
+  generatePuzzle() {
+    this.imagesList = [];
+    this.imagesSolve = [];
+    this.success = 0;
+    for(let i=0 ; i<=99 ; ++i) {
+      const [x, y] = [Math.floor(i/10), i%10];
+      const imgNo = ('0'+(x+y*10)).slice(-2);
+      this.imagesList.push(
+        {
+          x: 0,
+          y:0,
+          xi: x,
+          yi: y,
+          imgUrl: `../assets/image/${this.puzzleImage}/${imgNo}.jpeg`,
+          imgCode: imgNo
+        }
+      )
+      this.imagesSolve.push(
+        {
+          xi: x,
+          yi: y,
+        }
+      );
+    }
+  }
+
+  ngAfterViewInit() {
+    this.getPosition();
+  }
+  
+  getPosition(): void {
+    if(this.elementRef)
+    this.containerPos = this.elementRef.nativeElement.getBoundingClientRect();
+  }
+
+  cdkDragEnded(pos: any){
+    const [yi, xi] = pos.event.srcElement.currentSrc.split('/').slice(-1)[0].split('.')[0].split('');
+    
+    console.log("Hi");
+    const y = Math.floor((pos.dropPoint.x - this.containerPos.x - 2)/62);
+    const x = Math.floor((pos.dropPoint.y - this.containerPos.y - 2)/42);
+    const idx = this.imagesList.findIndex((img: any) => (img.xi === x && img.yi === y));
+
+    if(!(xi == x && yi == y)) return;
+    const obj = this.imagesSolve.find((img: any) => (img.xi === x && img.yi === y));
+    if(idx === -1)  return;
+
+    obj.imgUrl = this.imagesList[idx].imgUrl;
+    this.imagesList.splice(idx, 1);
+    this.success++;
+  }
+
+  toggleChange(e: any): void{ 
+    this.checked = e.checked;
+  }
+
+  onPuzzleImageChange(){
+    this.generatePuzzle();
+  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { FormsModule } from '@angular/forms';
+import { MatRadioModule } from '@angular/material/radio';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    DragDropModule,
+    MatSlideToggleModule,
+    NgSelectModule,
+    FormsModule,
+    MatRadioModule
   ],
   providers: [],
   bootstrap: [AppComponent]
